22890c70736d27ce98e66c389009394e23820497
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-05 04:33:07 +0530

    feat: initalized quiz page

---
+++ pages/quizes/[quizId].tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import Seo from "../../components/Seo";
+import Layout from "../../components/Layout";
+
+const Quiz = () => {
+	const router = useRouter();
+	const { quizId } = router.query;
+	console.log(quizId);
+	return (
+		<Layout>
+			<Seo title={`Quiz ${quizId}`} />
+			<main>
+				<h1 className=" px-5  text-red-400 smest:text-blue-500 smer:text-orange-200 ">
+					this is quiz {quizId}
+				</h1>
+			</main>
+		</Layout>
+	);
+};
+
+export default Quiz;
