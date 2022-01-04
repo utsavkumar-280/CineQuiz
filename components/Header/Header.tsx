e8f21d3ad1bddded60a46c84a6d8655ce3a8677c
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-04 15:09:19 +0530

    initial setup

---
+++ components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+	return (
+		<div className={styles.head_container}>
+			<h1>Hello </h1>
+		</div>
+	);
+};
+
+export default Header;
+
+/*
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+*/
