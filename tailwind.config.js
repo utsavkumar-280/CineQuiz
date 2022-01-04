f24b57a508fbc6c14ac6afb80068fbe4184cd56f
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-05 04:28:13 +0530

    chore: added custom tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+			},
+
+			colors: {
+				themeBg: "#051c25",
+			},
+		},
+		screens: {
+			smest: "360px",
+			smer: "411px",
+			...defaultTheme.screens,
+		},
 	},
 	plugins: [],
 };
