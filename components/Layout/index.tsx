235c9d842c796dbf46257d743d427da11b8c43f9
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-06 20:16:55 +0530

    feat: added responsiveness

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,13 +4,14 @@ import Footer from "../Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="min-h-screen w-full font-inter transition-all ease duration-300">
-			<div className=" min-h-screen flex flex-col justify-start">
-				<Header />
-				{children}
+		<div className="min-h-screen w-full flex justify-center font-inter transition-all ease duration-300 ">
+			<div className=" w-[95%] flex flex-col sm:w-11/12 md:w-9/12 lg:w-8/12 2xl:w-7/12">
+				<div className="w-full min-h-screen flex flex-col justify-start">
+					<Header />
+					{children}
+				</div>
+				<Footer />
 			</div>
-
-			<Footer />
 		</div>
 	);
 };
