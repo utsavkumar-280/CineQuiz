a27c5eee516f58b4d9fa0823f7c4d1002c3700ff
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-08 22:00:21 +0530

    refact: changed some content and layout

--- components/DashboardCard/index.tsx
+++ components/DashboardCard/index.tsx
@@ -1,28 +1,32 @@
 import { useState } from "react";
+import Link from "next/link";
+
 const DashboardCard = () => {
 	const [isCorrect, setCorrect] = useState(true);
 	return (
-		<div className="w-full flex justify-between items-center bg-themeCard mb-5 p-4  rounded-xl">
-			<div>
-				<h1 className="text-neutral-300 text-lg lg:text-xl font-semibold tracking-wide">
-					Ultimate Harry Potter Quiz
-				</h1>
-				<button
-					className=" text-gray-500 hover:text-green-400 transition-all ease duration-[250ms]"
-					onClick={() => setCorrect((prev) => !prev)}
-				>
-					Retake
-				</button>
+		<div className="w-full  flex flex-col justify-between sm:flex-row items-center bg-themeCard mb-5 p-3 sm:p-4  rounded-xl">
+			<div className="h-20 smer:h-16 flex flex-col justify-between items-start self-start sm:self-center">
+				<Link href={`/quizes/1`}>
+					<a
+						className="text-neutral-300 text-lg lg:text-xl font-semibold tracking-wide hover:text-green-400 transition-all ease duration-[250ms]"
+						onClick={() => setCorrect((prev) => !prev)}
+					>
+						Lord of the Rings: Test of the Triology
+					</a>
+				</Link>
+				<Link href={`/result`}>
+					<a className=" text-gray-500 hover:text-green-400 transition-all ease duration-[250ms]">
+						Result
+					</a>
+				</Link>
 			</div>
 			<div
 				className={`flex ${
-					isCorrect
-						? "text-neutral-100 bg-green-600"
-						: "text-neutral-100 bg-red-600"
-				}    rounded-full `}
+					isCorrect ? "text-green-400 " : "text-red-500 "
+				}    rounded-full  self-end sm:self-center`}
 			>
-				<p className="pr-1 pl-3">40/100</p>
-				<p className="pl-2 pr-3">Failed</p>
+				<p className="pr-1 ">Score: </p>
+				<p className="pl-1 ">40/100</p>
 			</div>
 		</div>
 	);
