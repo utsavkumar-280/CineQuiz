49c7c606af8c4d8c616c1b503eac86c032c6d38c
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-10 06:01:19 +0530

    feat: added quiz data context and provider to the whole tree

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QuizDataProvider } from "../contexts/QuizDataProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Component {...pageProps} />
+			<QuizDataProvider>
+				<Component {...pageProps} />
+			</QuizDataProvider>
 		</>
 	);
 }
