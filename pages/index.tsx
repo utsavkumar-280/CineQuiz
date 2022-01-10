52fc0f6eb524d0b291321b3ee6d504c65cac7611
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-10 13:21:06 +0530

    feat: index page will use ISR now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import { useEffect } from "react";
-import type { NextPage } from "next";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -11,19 +9,20 @@ import { categories } from "../utils/data";
 
 import { useQuizData } from "../contexts/QuizDataProvider";
 import { resetQuizState } from "../reducers/quiz.reducer";
+import { Quiz } from "../utils";
 
-const Home: NextPage = () => {
+const Home = ({ quizzes }: { quizzes: Quiz[] }) => {
 	const router = useRouter();
 	const searchedCategory = router?.query?.cat || "All";
 
-	const { state, dispatch } = useQuizData();
+	const { dispatch } = useQuizData();
 
 	useEffect(() => {
 		dispatch(resetQuizState());
 	}, [dispatch]);
 	return (
 		<Layout>
-			<Seo title={`Home`} />
+			<Seo />
 			<main className="w-full grow flex flex-col">
 				<div className="w-full bg-themeBg h-16 flex items-center z-10 sticky top-16 sm:top-20">
 					<nav className="w-full flex justify-start sm:justify-center flex-nowrap overflow-x-auto whitespace-nowrap tracking-wide">
@@ -47,8 +46,8 @@ const Home: NextPage = () => {
 
 				<div className="grow px-9 flex flex-col items-center">
 					{searchedCategory === "All"
-						? state.quizzes?.map((q) => <QuizCard quiz={q} key={q._id} />)
-						: state.quizzes
+						? quizzes?.map((q) => <QuizCard quiz={q} key={q._id} />)
+						: quizzes
 								?.filter((quiz) => quiz.category === searchedCategory)
 								.map((q) => <QuizCard quiz={q} key={q._id} />)}
 				</div>
@@ -58,3 +57,16 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+	const response = await fetch("https://cine-insta.herokuapp.com/quizzes");
+	if (response.status === 200) {
+		const data = await response.json();
+		return {
+			props: {
+				quizzes: data.response,
+			},
+			revalidate: 10,
+		};
+	}
+};
