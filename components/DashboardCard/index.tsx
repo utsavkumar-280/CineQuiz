d3d068561ada665432756a3323e9d2acc0375414
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-10 11:36:21 +0530

    feat: added the data and links to dashboard page

--- components/DashboardCard/index.tsx
+++ components/DashboardCard/index.tsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
 import Link from "next/link";
+import { Attempts } from "../../reducers/reducer.types";
 
-const DashboardCard = () => {
-	const [isCorrect, setCorrect] = useState(true);
+const DashboardCard = ({ attempt }: { attempt: Attempts }) => {
 	return (
 		<div className="w-full  flex flex-col justify-between sm:flex-row items-center bg-themeCard mb-5 p-3 sm:p-4  rounded-xl">
 			<div className="h-20 smer:h-16 flex flex-col justify-between items-start self-start sm:self-center">
-				<Link href={`/quizes/1`}>
-					<a
-						className="text-neutral-300 text-lg lg:text-xl font-semibold tracking-wide hover:text-green-400 transition-all ease duration-[250ms]"
-						onClick={() => setCorrect((prev) => !prev)}
-					>
-						Lord of the Rings: Test of the Triology
+				<Link href={`/quizes/${attempt.quiz?._id}`}>
+					<a className="text-neutral-300 text-lg lg:text-xl font-semibold tracking-wide hover:text-green-400 transition-all ease duration-[250ms]">
+						{attempt.quiz?.name}
 					</a>
 				</Link>
-				<Link href={`/result`}>
+				<Link href={`/result/?attemptId=${encodeURI(attempt._id)}`}>
 					<a className=" text-gray-500 hover:text-green-400 transition-all ease duration-[250ms]">
 						Result
 					</a>
@@ -22,11 +19,13 @@ const DashboardCard = () => {
 			</div>
 			<div
 				className={`flex ${
-					isCorrect ? "text-green-400 " : "text-red-500 "
+					attempt.score > 0 ? "text-green-400 " : "text-red-500 "
 				}    rounded-full  self-end sm:self-center`}
 			>
 				<p className="pr-1 ">Score: </p>
-				<p className="pl-1 ">40/100</p>
+				<p className="pl-1 ">
+					{attempt.score}/{attempt.quiz?.totalScore}
+				</p>
 			</div>
 		</div>
 	);
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,26 +1,52 @@
-import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 import DashboardCard from "../components/DashboardCard";
 
+import { useQuizData } from "../contexts/QuizDataProvider";
+import {
+	clearAllPreviousAttempts,
+	resetQuizState,
+} from "../reducers/quiz.reducer";
+
 const Dashboard = () => {
+	const {
+		state: { previousAttempts },
+		dispatch,
+	} = useQuizData();
+
+	useEffect(() => {
+		dispatch(resetQuizState());
+	}, [dispatch]);
+
 	return (
 		<Layout>
 			<Seo title={`Dashboard`} />
 			<main className="w-full grow px-2 flex flex-col items-center">
-				<section className="w-full xl:w-9/12 lg:w-10/12 flex justify-between items-center h-16 sm:h-20">
-					<h1 className="text-lg text-neutral-300">Previous Attempt</h1>
-					<button className=" text-gray-500 border border-gray-800 px-3 rounded-full hover:text-green-400 hover:border-green-400 transition-all ease duration-[250ms]">
-						Clear All
-					</button>
-				</section>
-				<section className="grow w-full xl:w-9/12 lg:w-10/12 flex flex-col items-center">
-					<DashboardCard />
-					<DashboardCard />
-					<DashboardCard />
-					<DashboardCard />
-				</section>
+				{previousAttempts && previousAttempts.length != 0 ? (
+					<>
+						<section className="w-full xl:w-9/12 lg:w-10/12 flex justify-between items-center h-16 sm:h-20">
+							<h1 className="text-lg text-neutral-300">Previous Attempt</h1>
+							<button
+								className=" text-gray-500 border border-gray-800 px-3 rounded-full hover:text-green-400 hover:border-green-400 transition-all ease duration-[250ms]"
+								onClick={() => dispatch(clearAllPreviousAttempts())}
+							>
+								Clear All
+							</button>
+						</section>
+						<section className="grow w-full xl:w-9/12 lg:w-10/12 flex flex-col items-center">
+							{previousAttempts
+								.slice(0)
+								.reverse()
+								.map((attempt) => (
+									<DashboardCard key={attempt._id} attempt={attempt} />
+								))}
+						</section>
+					</>
+				) : (
+					<div>No previous Attempts</div>
+				)}
 			</main>
 		</Layout>
 	);
